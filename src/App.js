f246167cce976b2f68221d4eea4c0815dd69f914
rezwanulislam114 <rezwanulislam114@gmail.com>
2021-10-18 01:04:37 +0600

    firebase setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import Home from './components/Home/Home';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
+import AuthProvider from './AuthProvider/AuthProvider';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDeCvFblZbzWrCDO8jqqIR2dkmBByuvEbY",
+    authDomain: "alpha-care-d0392.firebaseapp.com",
+    projectId: "alpha-care-d0392",
+    storageBucket: "alpha-care-d0392.appspot.com",
+    messagingSenderId: "1088497567093",
+    appId: "1:1088497567093:web:849ff70109878889c97f3c"
+};
+
+export default firebaseConfig;
