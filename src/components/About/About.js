d18f14d4dea4c2da14615d9f1f9751f86d417aa0
rezwanulislam114 <rezwanulislam114@gmail.com>
2021-10-18 15:43:59 +0600

    review section added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Carousel, Col, Container, Row } from 'react-bootstrap';
 import SingleAbout from '../SingleAbout/SingleAbout';
 import aboutIntro from '../../images/about/about-intro.svg';
+import './About.css'
+import reviewBg from '../../images/about/review.png'
+import person1 from '../../images/about/person-1.jpg'
+import person2 from '../../images/about/person-2.jpg'
+import person3 from '../../images/about/person-3.jpg'
 
 const About = () => {
     // data for about section 
@@ -25,7 +30,7 @@ const About = () => {
             <Container className="mt-5">
                 {/* about us section  */}
                 <h1 className="title">A Little View What We Do</h1>
-                <Row xs={1} md={2} className="d-flex align-items-center">
+                <Row xs={1} md={2} className="d-flex align-items-center mb-5">
                     <Col>
                         <img className="img-fluid p-4" src={aboutIntro} alt="" />
                     </Col>
@@ -39,10 +44,78 @@ const About = () => {
                 </Row>
 
                 {/* image gallary section  */}
-                <div>
+                <h1 className="title">Take A Little Idea</h1>
+                <div className="gallary my-5">
                     {
-                        gallaries.map(item => <img key={item.id} src={item.img} alt="" />)
+                        gallaries.map(item => <div className="gallary-image"><img key={item.id} src={item.img} alt="" /></div>)
                     }
+                </div>
+
+                {/* patiant reviews */}
+                <div>
+                    <Row xs={1} md={2} className="d-flex align-items-center my-5">
+                        <Col>
+                            <h1 className="title">Thoughts from our best <br /> pacient’s experience</h1>
+                            <p>Seeking for verbal of our service quality? Find them here. Everything is transparent and straightforward for your sense of jusitifcation.</p>
+                        </Col>
+                        <Col>
+                            <Carousel variant="dark" className="m-2">
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100"
+                                        src={reviewBg}
+                                        alt="First slide"
+                                    />
+                                    <Carousel.Caption className="review">
+                                        <p className="text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                        <div className="text-start">
+                                            <img src={person1} alt="" />
+                                            <div>
+                                                <h3>Macalave Jakes</h3>
+                                                <p>Maneger, IFIC Bank</p>
+                                            </div>
+                                        </div>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100"
+                                        src={reviewBg}
+                                        alt="Second slide"
+                                    />
+
+                                    <Carousel.Caption className="review">
+                                        <p className="text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                        <div className="text-start">
+                                            <img src={person2} alt="" />
+                                            <div>
+                                                <h3>Jons Harry</h3>
+                                                <p>Engineer, Google</p>
+                                            </div>
+                                        </div>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100"
+                                        src={reviewBg}
+                                        alt="Third slide"
+                                    />
+
+                                    <Carousel.Caption className="review">
+                                        <p className="text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                        <div className="text-start">
+                                            <img src={person3} alt="" />
+                                            <div>
+                                                <h3>Natasha Jarry</h3>
+                                                <p>Maneger, XFc Private Company Ltd</p>
+                                            </div>
+                                        </div>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                            </Carousel>
+                        </Col>
+                    </Row>
                 </div>
             </Container>
         </div>
